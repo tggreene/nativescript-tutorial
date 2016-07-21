52736e8e32baf92cc9e88037f3d9d622733c3462
Tim Greene <tim.g.greene@gmail.com>
2016-07-21 16:45:35 +0100

    add angular bindings to login data

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,33 @@ import {Component} from "@angular/core";
   selector: "my-app",
   template: `
     <StackLayout>
+      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
       <TextField hint="Email Address"
                 keyboardType="email"
                 autocorrect="false"
-                autocapitalizationType="none"></TextField>
+                autocapitalizationType="none"
+                [(ngModel)]="email"></TextField>
       <TextField hint="Password" secure="true"></TextField>
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign up for Groceries"></Button>
+      <Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'"
+              class="submit-button"
+              (tap)="submit()"></Button>
+      <Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'"
+              (tap)="toggleDisplay()"></Button>
     </StackLayout>
   `,
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 
-export class AppComponent {}
+export class AppComponent {
+  email = "foo@bar.baz";
+  isLoggingIn = true;
+
+  submit() {
+    console.log("Hello!");
+    alert("You're using " + this.email);
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
